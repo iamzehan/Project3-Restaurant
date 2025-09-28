b105baed2768d90389112f2bd91deb116b4311e2
Zehan <zehan.kareem@gmail.com>
2025-09-29 05:34:20 +0600

    Added to content div with Javascript

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import './testStyle.css';
 
+const content = document.querySelector("#content");
+
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button)=> {
     button.classList.add("icon");
 })
+
+const mainIcon = document.createElement("span");
+mainIcon.classList.add("icon");
+mainIcon.textContent = 'chef_hat';
+content.appendChild(mainIcon);
+const headline = document.createElement("h2");
+headline.classList.add("headline");
+headline.textContent = "Welcome to Restoura";
+content.appendChild(headline);
+const description = document.createElement("p");
+content.appendChild(description);
+
+
+
