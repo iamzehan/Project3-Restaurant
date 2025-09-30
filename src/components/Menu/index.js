c265dff8bafdd6bcf51eab25a76a6c452f54b1f1
Zehan <zehan.kareem@gmail.com>
2025-10-01 04:21:50 +0600

    Made Card Creation dynamic

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,14 +1,12 @@
 import "./style.css";
 import Data from "./menu.json";
 
-const coffeeList = document.createElement("ul");
-coffeeList.classList.add("coffee");
+const createCard = (key, values) => {
 
-const teaList = document.createElement("ul");
-teaList.classList.add("tea");
-
-const createCard = (value, list) => {
-  const li = document.createElement("li");
+  const list = document.createElement("ul");
+  list.classList.add(key);
+  values.forEach((value)=> {
+    const li = document.createElement("li");
   li.id = value.id;
   const img = document.createElement("img");
   img.src = value.image_url;
@@ -17,36 +15,34 @@ const createCard = (value, list) => {
   li.appendChild(img);
   li.appendChild(h2);
   const price = document.createElement("span");
-  price.classList.add("price")
+  price.classList.add("price");
   li.appendChild(price);
-  if (typeof(value.price) === 'number'){
+  if (typeof value.price === "number") {
     price.textContent = value.price;
+  } else {
+    console.log(value);
+    value.size.forEach((size) => {
+      const div = document.createElement("div");
+      const h5 = document.createElement("h5");
+      const h6 = document.createElement("h6");
+      h5.textContent = size;
+      h6.textContent = value.price[size];
+      div.appendChild(h5);
+      div.appendChild(h6);
+      price.appendChild(div);
+    });
   }
-  else {
-  value.size.forEach((size) => {
-    const div = document.createElement("div");
-    const h5 = document.createElement("h5");
-    const h6 = document.createElement("h6");
-    h5.textContent = size;
-    h6.textContent = value.price[size];
-    div.appendChild(h5);
-    div.appendChild(h6);
-    price.appendChild(div);
-  });
-}
   list.appendChild(li);
+  });
+  return list;
+  
 };
 
-Data.menu.coffee.forEach((value) => {
-    createCard(value, coffeeList);
-});
-
-Data.menu.tea.forEach((value) => {
-    createCard(value, teaList);
-});
 const Menu = document.createElement("div");
 Menu.classList.add("carousel");
-Menu.appendChild(coffeeList);
-Menu.appendChild(teaList);
+
+Object.keys(Data.menu).forEach((key) => {
+  Menu.appendChild(createCard(key, Data.menu[key]));
+});
 
 export default Menu;
