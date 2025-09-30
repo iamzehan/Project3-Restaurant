1d4bbc51ebf8d26083cdb3a65053f114e7301872
Zehan <zehan.kareem@gmail.com>
2025-09-30 23:06:50 +0600

    Wrapped in main function

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
 import "./testStyle.css";
 import Home from "./components/Home/index.js";
 
-const content = document.querySelector("#content");
+function main() {
+  const content = document.querySelector("#content");
 
-// Create page loader function
-const loadPage = (pageName, clicked = false) => {
-  content.innerHTML = "";
-  if (pageName == "Home") {
-    Home.forEach((item) => content.appendChild(item));
-  }
-  if (clicked) {
-    console.log(`${pageName} was clicked!`);
-  }
-};
+  // Create page loader function
+  const loadPage = (pageName, clicked = false) => {
+    content.innerHTML = "";
+    if (pageName == "Home") {
+      Home.forEach((item) => content.appendChild(item));
+    }
+    if (clicked) {
+      console.log(`${pageName} was clicked!`);
+    }
+  };
 
-// Load home page on page load
-document.addEventListener("DOMContentLoaded", function () {
-  loadPage("Home");
-});
-
-// get all the buttons
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach((button) => {
-  button.classList.add("icon");
-  // add events to navigation buttons
-  button.addEventListener("click", () => {
-    loadPage(button.textContent, true);
+  // Load home page on page load
+  document.addEventListener("DOMContentLoaded", function () {
+    loadPage("Home");
   });
-});
+
+  // get all the buttons
+  const buttons = document.querySelectorAll("button");
+
+  buttons.forEach((button) => {
+    button.classList.add("icon");
+    // add events to navigation buttons
+    button.addEventListener("click", () => {
+      loadPage(button.textContent, true);
+    });
+  });
+}
+ main();
