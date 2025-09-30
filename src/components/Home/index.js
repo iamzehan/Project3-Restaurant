990d361cc57ba8c9b4ad32362a3b32bc4093e870
Zehan <zehan.kareem@gmail.com>
2025-09-30 22:41:29 +0600

    Some minor changes

---
+++ src/components/Home/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+export const mainIcon = document.createElement("span");
+mainIcon.classList.add("icon");
+mainIcon.textContent = "chef_hat";
+export const headline = document.createElement("h2");
+headline.classList.add("headline");
+headline.textContent = "Welcome to Restoura";
+export const description = document.createElement("p");
+
--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import './testStyle.css';
+import { mainIcon, headline, description} from './components/Home/index.js';
 
 const content = document.querySelector("#content");
+
 
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button)=> {
     button.classList.add("icon");
 })
-
-const mainIcon = document.createElement("span");
-mainIcon.classList.add("icon");
-mainIcon.textContent = 'chef_hat';
-content.appendChild(mainIcon);
-const headline = document.createElement("h2");
-headline.classList.add("headline");
-headline.textContent = "Welcome to Restoura";
-content.appendChild(headline);
-const description = document.createElement("p");
-content.appendChild(description);
 
 
 
