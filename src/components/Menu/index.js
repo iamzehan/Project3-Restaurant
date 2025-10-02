97a97df6e16886d1331eac9b56dbce07d7a72753
Zehan <zehan.kareem@gmail.com>
2025-10-02 19:50:10 +0600

    Price processor added

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,34 @@
 import "./style.css";
 import Data from "./menu.json";
 
+const priceProcessor = (value,price) => {
+  if (typeof value.price === "number") {
+    const div = document.createElement("div");
+    div.classList.add("single");
+    const h5 = document.createElement("h5");
+    const h6 = document.createElement("h6");
+    h5.textContent = "Price";
+    h6.textContent = value.price;
+    div.appendChild(h5);
+    div.appendChild(h6);
+    console.log(typeof div);
+    price.appendChild(div);
+  } else {
+    value.size.forEach((size) => {
+      const div = document.createElement("div");
+      div.classList.add("multiple");
+      const h5 = document.createElement("h5");
+      const h6 = document.createElement("h6");
+      h5.textContent = size;
+      h6.textContent = value.price[size];
+      div.appendChild(h5);
+      div.appendChild(h6);
+      console.log(typeof div);
+      price.appendChild(div);
+    });
+  }
+};
+
 const createCard = (key, values) => {
   const list = document.createElement("ul");
   list.classList.add(key);
@@ -15,31 +43,8 @@ const createCard = (key, values) => {
     li.appendChild(h2);
     const price = document.createElement("div");
     price.classList.add("price");
+    priceProcessor(value, price);
     li.appendChild(price);
-    
-    if (typeof value.price === "number") {
-      const div = document.createElement("div");
-      div.classList.add("single");
-      const h5 = document.createElement("h5");
-      const h6 = document.createElement("h6");
-      h5.textContent = "Price";
-      h6.textContent = value.price;
-      div.appendChild(h5);
-      div.appendChild(h6);
-      price.appendChild(div);
-    } else {
-      value.size.forEach((size) => {
-        const div = document.createElement("div");
-        div.classList.add("multiple");
-        const h5 = document.createElement("h5");
-        const h6 = document.createElement("h6");
-        h5.textContent = size;
-        h6.textContent = value.price[size];
-        div.appendChild(h5);
-        div.appendChild(h6);
-        price.appendChild(div);
-      });
-    }
     list.appendChild(li);
   });
   return list;
