eda87d0aa5c0e80ec5ed307dde43c999ccde56d9
Zehan <zehan.kareem@gmail.com>
2025-10-02 21:52:52 +0600

    Major Refactoring

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,49 +1,53 @@
 import "./style.css";
 import Data from "./menu.json";
 
-const priceProcessor = (value, price) => {
-  function createPrice(classType, header5, header6) {
+const createCard = (value, list) => {
+  function arrangePrice(classType, category, price) {
     const div = document.createElement("div");
     div.classList.add(classType);
     const h5 = document.createElement("h5");
     const h6 = document.createElement("h6");
-    h5.textContent = header5;
-    h6.textContent = header6;
+    h5.textContent = category;
+    h6.textContent = price;
     div.appendChild(h5);
     div.appendChild(h6);
-    price.appendChild(div);
+    return div;
   }
+  
+  const li = document.createElement("li");
+  li.id = value.id;
+
+  const img = document.createElement("img");
+  img.src = value.image_url;
+  li.appendChild(img);
+
+  const h2 = document.createElement("h2");
+  h2.textContent = value.name;
+  li.appendChild(h2);
+
+  const price = document.createElement("div");
+  price.classList.add("price");
 
   if (typeof value.price === "number") {
-    createPrice("single", "Price", value.price);
+    let div = arrangePrice("single", "Price", value.price);
+    price.appendChild(div);
   } else {
     value.size.forEach((size) => {
-      createPrice("multiple", size, value.price[size]);
+      let div = arrangePrice("multiple", size, value.price[size]);
+      price.appendChild(div);
     });
   }
+
+  li.appendChild(price);
+
+  list.appendChild(li);
 };
 
-const createCard = (key, values) => {
+const createMenu = (key, values) => {
   const list = document.createElement("ul");
   list.classList.add(key);
   values.forEach((value) => {
-    const li = document.createElement("li");
-    li.id = value.id;
-
-    const img = document.createElement("img");
-    img.src = value.image_url;
-    li.appendChild(img);
-
-    const h2 = document.createElement("h2");
-    h2.textContent = value.name;
-    li.appendChild(h2);
-
-    const price = document.createElement("div");
-    price.classList.add("price");
-    priceProcessor(value, price);
-    li.appendChild(price);
-
-    list.appendChild(li);
+    createCard(value, list);
   });
   return list;
 };
@@ -52,7 +56,7 @@ const Menu = document.createElement("div");
 Menu.classList.add("carousel");
 
 Object.keys(Data.menu).forEach((key) => {
-  Menu.appendChild(createCard(key, Data.menu[key]));
+  Menu.appendChild(createMenu(key, Data.menu[key]));
 });
 
 export default Menu;
